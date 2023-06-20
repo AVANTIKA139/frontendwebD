1098c6faa6a30c9799921db8f9ce560a90c56a9e
Avantika tiwari <avantikatiwari18@gmail.com>
2023-06-20 19:26:46 +0530

    ejh

---
+++ src/Heading.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+function Heading() {
+  return <h1>my name is Avantika</h1>;
+}
+export default Heading;
---
+++ src/List.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function List() {
+  return (
+    <ol>
+      <li>avantika</li>
+      <li>rani</li>
+      <li>sania</li>
+    </ol>
+  );
+}
+export default List;
